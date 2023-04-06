f44646830fa49f7ae0916d0e9e26293393584636
Yuhei <yuhei61627@icloud.com>
2023-04-05 23:21:04 -0700

    Debugged web server

--- frontend/src/Components/Extra Components/FilterDepartment.js
+++ frontend/src/Components/Extra Components/FilterDepartment.js
@@ -1,19 +1,19 @@
-import React from "react";
-import {Stack} from "@mui/material";
-import DepartmentSearch from "/Users/Daichi/project_team004/frontend/src/Components/Extra Components/DepartmentSearch";
-import Sidebar from "/Users/Daichi/project_team004/frontend/src/Components/Sidebar";
-import Navbar from "/Users/Daichi/project_team004/frontend/src/Components/Navbar";
-
-const FilterProfessor = () => {
-	return (
-		<div className="filter_profs">
-			<Navbar />
-			<Stack direction={"row"} spacing={1} justifyContent="space-between">
-				<Sidebar />
-				<DepartmentSearch />
-			</Stack>
-		</div>
-	);
-};
-
-export default FilterProfessor;
+// import React from "react";
+// import {Stack} from "@mui/material";
+// import DepartmentSearch from "/Users/Daichi/project_team004/frontend/src/Components/Extra Components/DepartmentSearch";
+// import Sidebar from "/Users/Daichi/project_team004/frontend/src/Components/Sidebar";
+// import Navbar from "/Users/Daichi/project_team004/frontend/src/Components/Navbar";
+//
+// const FilterProfessor = () => {
+// 	return (
+// 		<div className="filter_profs">
+// 			<Navbar />
+// 			<Stack direction={"row"} spacing={1} justifyContent="space-between">
+// 				<Sidebar />
+// 				<DepartmentSearch />
+// 			</Stack>
+// 		</div>
+// 	);
+// };
+//
+// export default FilterProfessor;
--- frontend/src/Components/Extra Components/FilterProfessors.js
+++ frontend/src/Components/Extra Components/FilterProfessors.js
@@ -1,19 +1,19 @@
-import React from "react";
-import {Stack} from "@mui/material";
-import ProfessorSearch from "/Users/Daichi/project_team004/frontend/src/Components/Extra Components/ProfessorSearch";
-import Sidebar from "/Users/Daichi/project_team004/frontend/src/Components/Sidebar";
-import Navbar from "/Users/Daichi/project_team004/frontend/src/Components/Navbar";
-
-const FilterProfessor = () => {
-	return (
-		<div className="filter_profs">
-			<Navbar />
-			<Stack direction={"row"} spacing={1} justifyContent="space-between">
-				<Sidebar />
-				<ProfessorSearch />
-			</Stack>
-		</div>
-	);
-};
-
-export default FilterProfessor;
+// import React from "react";
+// import {Stack} from "@mui/material";
+// import ProfessorSearch from "/Users/Daichi/project_team004/frontend/src/Components/Extra Components/ProfessorSearch";
+// import Sidebar from "/Users/Daichi/project_team004/frontend/src/Components/Sidebar";
+// import Navbar from "/Users/Daichi/project_team004/frontend/src/Components/Navbar";
+//
+// const FilterProfessor = () => {
+// 	return (
+// 		<div className="filter_profs">
+// 			<Navbar />
+// 			<Stack direction={"row"} spacing={1} justifyContent="space-between">
+// 				<Sidebar />
+// 				<ProfessorSearch />
+// 			</Stack>
+// 		</div>
+// 	);
+// };
+//
+// export default FilterProfessor;
